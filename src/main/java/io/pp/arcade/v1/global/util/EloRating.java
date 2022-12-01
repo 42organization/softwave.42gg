298106d438b71ef5766ae38498d4a8bf2a57c200
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-12-01 11:55:53 +0900

    [fix] 점수 차에 따른 추가 pppchange 적용 close #47

--- src/main/java/io/pp/arcade/v1/global/util/EloRating.java
+++ src/main/java/io/pp/arcade/v1/global/util/EloRating.java
@@ -1,12 +1,11 @@
 package io.pp.arcade.v1.global.util;
 
 public class EloRating {
-    public static Integer pppChange(Integer myPPP, Integer opponentPPP, Boolean isWin, Boolean isOneSide) {
+    public static Integer pppChange(Integer myPPP, Integer opponentPPP, Boolean isWin, Integer difference) {
         Double we = 1.0 / (Math.pow(10.0, (opponentPPP - myPPP) / 400.0) + 1.0);
         Double change = 40 * ((isWin ? 1 : 0) - we);
-        if (isOneSide) {
-            change = change + change * 0.21;
-        }
+        change = change + (change * difference * 0.1);
+
         if (change < 0) {
             change = change * 0.9;
         }
